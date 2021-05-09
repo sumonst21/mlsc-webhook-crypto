0be4822c26980ea677422135a02a6c8690c0acb3
Murillo Costa <costamurillo88@gmail.com>
2021-05-09 18:41:30 +1000

    breakout function deployment

--- app.py
+++ app.py
@@ -7,15 +7,21 @@ app = Flask(__name__)
 
 client = Client(config.API_KEY, config.API_SECRET)
 
-def order(side, quantity, symbol, order_type=ORDER_TYPE_MARKET):
+# general functions
+def order(side, quantity, symbol, order_type=ORDER_TYPE_MARKET, stopPrice=0):
+    
     try:
-        print(f"sending order {order_type} - {side} {quantity} {symbol}")
-        order = client.create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
+        # print(f"sending order {order_type} - {side} {quantity} {symbol}")
+        order = client.futures_create_order(symbol=symbol, side=side, type=order_type, quantity=quantity, stopPrice=stopPrice)
     except Exception as e:
         print("an exception occured - {}".format(e))
         return False
 
     return order
+
+def percentage_change(start_value, end_value):
+    return float(abs(start_value - end_value) / start_value)
+
 
 @app.route('/')
 def welcome():
@@ -49,3 +55,67 @@ def webhook():
             "code": "error",
             "message": "order failed"
         }
+
+@app.route('/breakout', methods=['POST'])
+def breakout():
+
+    data = json.loads(request.data)
+
+    # check
+    if data['passphrase'] != config.WEBHOOK_PASSPHRASE:
+        return {
+            "code": "error",
+            "message": "Nice try, invalid passphrase"
+        }
+
+    # parse tradingview webhook data
+    take_profit_target = 2
+    symbol = data['symbol']
+    entry_price = data['open']
+    pv_high = data['pv_high']
+    pv_low  = data['pv_low']
+    risk = float(data['risk'])
+    side_main = "BUY" if data['go_long'] == 1 else "SELL"
+    side_cross = "SELL" if data['go_long'] == 1 else "BUY"
+    stop_loss_perc = percentage_change(entry_price, pv_low if side_main == "BUY" else pv_high)
+
+    # set leverage to maximum allowe
+    if client:
+        leverage = client.futures_change_leverage(symbol=symbol, leverage='75')
+        print('Initializing leverage for', symbol, ':', leverage['leverage'], 'x')
+ 
+    # set parameters to create binance order
+    if stop_loss_perc:
+
+        quantity = (risk / stop_loss_perc) / entry_price
+        stop_loss = pv_low if side_main == "BUY" else pv_high
+        take_profit = ((stop_loss_perc * take_profit_target)) * entry_price
+        take_profit = take_profit + entry_price if side_main == "BUY" else abs(take_profit - entry_price)
+
+        print(f'placing MARKET-{side_main} order for symbol {symbol} quantity {quantity} estimatedEntry {entry_price}')
+        order_main = order(side_main, quantity, symbol, order_type='MARKET')
+
+        if order_main:
+                
+            print(f'placing STOP_MARKET-{side_cross} order for symbol {symbol} quantity {quantity} stopPrice {stop_loss} ')
+            order_stop = order(side_cross, quantity, symbol, order_type='STOP_MARKET')
+
+            print(f'placing TAKE_PROFIT_MARKET-{side_cross} order for symbol {symbol} quantity {quantity} stopPrice {take_profit} ')
+            order_take = order(side_cross, quantity, symbol, order_type='TAKE_PROFIT_MARKET')
+
+        else:
+        
+            print("Error placing main MARKET order-{side_main} order for symbol {symbol} quantity {quantity} ")
+
+        if order_take:
+            return {
+                "code": "success",
+                "message": "order executed"
+            }
+        else:
+            print("order failed")
+
+    return {
+        "code": "error",
+        "message": "order failed"
+    }
